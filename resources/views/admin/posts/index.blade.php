c6994758beebc468c7ef2838b36a16909ffaa3cd
batnu <batnu_piro@hotmail.com>
2020-01-09 18:27:05 +0000

    Paso 12

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,8 +3,8 @@
 @section('header')
     <div class="row mb-2">
         <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Todas las publicaciones</h1>
-            <small>Listado completo de posts</small>
+            <h1 class="m-0 text-dark">Posts <small>Listado</small>
+            </h1>
         </div><!-- /.col -->
         <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -16,5 +16,27 @@
 @endsection
 
 @section('content')
-    <h3>Listado de posts</h3>
+    <table id="posts-table" class="table table-bordered table-striped">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Título</th>
+            <th>Extracto</th>
+            <th>Acciones</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach($posts as $post)
+                <tr>
+                    <td>{{$post->id}}</td>
+                    <td>{{$post->title}}</td>
+                    <td>{{$post->excerpt}}</td>
+                    <td>
+                        <a href="#" class="btn btn-xs btn-info"><i class="fa fa-pencil-alt"></i></a>
+                        <a href="#" class="btn btn-xs btn-danger"><i class="fa fa-trash"></i></a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
