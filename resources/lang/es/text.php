fced2c382393394bae6d2bc9b24ebb10e6574d1d
batnu <batnu_piro@hotmail.com>
2020-01-09 16:23:58 +0000

    Logueo

---
+++ resources/lang/es/text.php
@@ -0,0 +1,12 @@
+<?php
+
+return[
+    'login' => [
+        'header' => 'Identificate para acceder a tu sesión',
+        'remember' => 'Recuerdame',
+        'forgot' => 'He olvidado mi contraseña',
+        'button' => 'Acceder',
+        'email' => 'Correo electrónico',
+        'password' => 'Contraseña'
+    ],
+];
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,23 @@ Route::get('posts', function(){
     return view('welcome',compact('posts'));
 });
 
-Route::get('admin', function (){
+Route::get('home', function (){
    return view('admin.dashboard');
-});
+})->middleware('auth');
 
+/*
+ * Si ponemos Route::auth(); nos pondria todas las rutas de auth
+ * Como en este caso no nos interesa, ya que  solo queremos las rutas para logear y logout
+ *  hay que buscar las rutas de auth que estan en
+ * vendor/laravel/framework/src/Illuminate/Routing/Router.php
+ * */
+
+
+//Rutas de login
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+//Rutas de registro
+//$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+//$this->post('register', 'Auth\RegisterController@register');
