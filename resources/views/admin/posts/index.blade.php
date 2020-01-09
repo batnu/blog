b3827361018988701fe080c996bbfb844d45a9b6
batnu <batnu_piro@hotmail.com>
2020-01-09 17:59:12 +0000

    Paso 11

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,18 +1,20 @@
 @extends('admin.layouts.layout')
+
 @section('header')
     <div class="row mb-2">
         <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Dashboard</h1>
+            <h1 class="m-0 text-dark">Todas las publicaciones</h1>
+            <small>Listado completo de posts</small>
         </div><!-- /.col -->
         <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('admin.home') }}">Dashboard</a></li>
+                <li class="breadcrumb-item active">Posts</li>
             </ol>
         </div><!-- /.col -->
     </div><!-- /.row -->
 @endsection
 
 @section('content')
-    <h1>Usuario autenticado: {{ auth()->user()->name }}</h1>
-    <h1>Correo electrónico: {{ auth()->user()->email }}</h1>
+    <h3>Listado de posts</h3>
 @endsection
